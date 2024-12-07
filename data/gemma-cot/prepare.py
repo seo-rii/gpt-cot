8a3270aa65877179d4a8f6abe87a178f6f70a4c7
Dongyeongkim <dongyeongkim33@gmail.com>
2024-12-07 23:47:23 +0900

    updated

--- data/gemma-cot/prepare.py
+++ data/gemma-cot/prepare.py
@@ -6,7 +6,7 @@ from tqdm import tqdm
 import numpy as np
 import tiktoken
 from datasets import load_dataset # huggingface datasets
-
+from datasets import Dataset
 # number of workers in .map() call
 # good number to use is ~order number of cpu cores // 2
 num_proc = 8
@@ -20,10 +20,11 @@ enc = tiktoken.get_encoding("gpt2")
 
 if __name__ == '__main__':
     # takes 54GB in huggingface .cache dir, about 8M documents (8,013,769)
-    dataset = load_dataset("json", split="train", data_files="",num_proc=num_proc_load_dataset)
-
+    dataset = load_dataset("json", data_files="total_data.json", split="train", num_proc=num_proc_load_dataset)
     # owt by default only contains the 'train' split, so create a test split
-    split_dataset = dataset["train"].train_test_split(test_size=0.0005, seed=2357, shuffle=True)
+    dataset = Dataset.from_list(dataset['results'])
+    # dataset = dataset.rename_column("results", "text")
+    split_dataset = dataset.train_test_split(test_size=0.2, seed=2357, shuffle=True)
     split_dataset['val'] = split_dataset.pop('test') # rename the test split to val
 
     # this results in:
